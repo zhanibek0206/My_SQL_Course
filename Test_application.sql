c24da885f9979cea61f2ead7643dee0e2451734e
zhanibek0206 <106267955+zhanibek0206@users.noreply.github.com>
2022-10-29 22:55:03 +0600

    Add files via upload

--- Test_application.sql
+++ Test_application.sql
@@ -53,7 +53,7 @@ ORDER BY name DESC;
 SELECT * FROM student
 WHERE name IN ('Mike', 'Kate', 'Claire') ORDER BY name ASC;
 
-
+DROP TABLE student;
 -- CREATING COMPANY DATABASE
 
 -- Cоздание таблицы "Employee"
@@ -176,5 +176,59 @@ INSERT INTO works_with VALUES (105, 406, 130000);
 
 SELECT * FROM employee;
 
+-- MORE BASIC QUERIES
+
+-- Find all emplyees
+SELECT * from employee;
+
+-- Find all clients
+SELECT * FROM client;
+
+-- Find all employees ordered by salary
+SELECT * FROM employee ORDER BY salary DESC;
+
+-- Find all employees ordered by sex then name
+SELECT * FROM employee ORDER BY SEX, first_name, last_name;
+
+-- Find the first 5 employees in the table
+SELECT * FROM employee LIMIT 5;
+
+-- Find the first_name and last_name of all employees
+SELECT first_name, last_name FROM employee;
+
+-- Find forename and surname of all employees
+SELECT first_name AS forename, last_name AS surname FROM employee;
+
+-- Find out all the different branch_id
+SELECT DISTINCT branch_id FROM employee; 
 
 
+-- FUNCTIONS
+
+SELECT * FROM employee;
+
+-- Find number of all employees
+SELECT COUNT(emp_id) FROM employee;
+
+-- Find the number of female employees born after 1970
+SELECT COUNT(emp_id) FROM employee
+WHERE sex = 'F' AND  birth_date > '1971-01-01';
+
+-- Find the average salary of all employee's where sex is male
+SELECT AVG(salary) FROM employee
+WHERE sex = 'M';
+
+-- Find the sum of all employee's salaries
+SELECT SUM(salary) FROM employee;
+
+-- Find out how many males and females there are
+SELECT COUNT(sex) FROM employee;
+
+-- If you want to count how many males and females exactly, then use GROUP BY
+SELECT COUNT(sex), sex FROM employee
+GROUP BY sex;
+
+SELECT * FROM works_with;
+-- Find the total sales from each salesman
+SELECT SUM(total_sales), emp_id FROM works_with 
+GROUP BY emp_id;
